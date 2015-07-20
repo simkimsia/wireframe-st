bde3edb8569145f60729d4f316cd12247f868906
KimSia Sim <kimcity@gmail.com>
2015-07-20 19:33:31 +0800

    update to responsive style

--- app/js/b/bundle.js
+++ app/js/b/bundle.js
@@ -1,15 +1,14 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var test = require('./test');
-var peep = require('./peep');
-
-},{"./peep":2,"./test":3}],2:[function(require,module,exports){
-var _ = require('lodash');
-
-console.log(_);
-},{"lodash":"lodash"}],3:[function(require,module,exports){
 var $ = require('jquery');
 
-console.log($);
+$(document).ready(function() {
+	$('.jsMobileMenu').on('click', 'button', function (event) {
+		console.log(this);
+		var triggeredElement = $(this).attr('ref');
+		console.log($(triggeredElement));
+		$(triggeredElement).toggleClass('is-open');
+	})
+});
 },{"jquery":"jquery"}],"jquery":[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v2.1.3
--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
-var test = require('./test');
-var peep = require('./peep');
+var $ = require('jquery');
+
+$(document).ready(function() {
+	$('.jsMobileMenu').on('click', 'button', function (event) {
+		console.log(this);
+		var triggeredElement = $(this).attr('ref');
+		console.log($(triggeredElement));
+		$(triggeredElement).toggleClass('is-open');
+	})
+});
